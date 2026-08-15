17a94e5f400a053c7f077e20875759bec2557395
Kaushan <kaushanj@gmail.com>
2026-08-15 10:45:42 +0530

    init project

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "system-ui, sans-serif" }}>
+      <h1>Internal Portal</h1>
+      <p>Test</p>
+    </main>
+  );
+}
